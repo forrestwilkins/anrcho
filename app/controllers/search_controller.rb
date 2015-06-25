45ede3d9ec52a79d2fa834d484c56d4bfb0dbf5d
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-06-25 03:07:53 -0400

    search_match

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,20 +8,22 @@ class SearchController < ApplicationController
     if @query.present?
       [Proposal, Comment, Group].each do |_class|
         _class.all.each do |item|
+          match = false
           # scans all items for matching tags
           item.hashtags.each do |tag|
             if @query.eql? tag.tag or "##{@query}".eql? tag.tag
-              @results << item
+              match = true
             end
           end
           # searches by token
           if @query.eql? item.token
-            @results << item
+            match = true
           end
           # searches by location
           if item.location.to_s.include? @query.to_s
-            @results << item
+            match = true
           end
+          @results << item if match
         end
       end
     end
