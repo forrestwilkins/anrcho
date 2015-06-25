5a54f1f0c0ec08a097228625e6fbd7f6430d0715
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-06-25 03:47:39 -0400

    placeholder_font_size

--- bin/bash/fresh.sh
+++ bin/bash/fresh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh root@anrcho.com 'service unicorn stop && cd /home/rails && git pull && export PATH=$PATH:/usr/local/rvm/rubies/ruby-2.2.1/bin && /home/rails/bin/bundle install && /home/rails/bin/rake assets:precompile RAILS_ENV=production && service unicorn start'
+ssh root@anrcho.com 'service unicorn stop && cd /home/rails && git pull && export PATH=$PATH:/usr/local/rvm/rubies/ruby-2.2.1/bin && /home/rails/bin/rake assets:precompile RAILS_ENV=production && service unicorn start'
