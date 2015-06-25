e10789d37e7fafc9b5b12eb8ee50a5099986c575
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-06-25 03:04:25 -0400

    avatar_link_fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,17 +5,23 @@ class SearchController < ApplicationController
   def index
     @query = params[:query].present? ? params[:query] : session[:query]
     session[:query] = @query; @results = []
-    [Proposal, Comment, Group].each do |_class|
-      _class.all.each do |item|
-        # scans all items for matching tags
-        item.hashtags.each do |tag|
-          if @query.eql? tag.tag or "##{@query}".eql? tag.tag
+    if @query.present?
+      [Proposal, Comment, Group].each do |_class|
+        _class.all.each do |item|
+          # scans all items for matching tags
+          item.hashtags.each do |tag|
+            if @query.eql? tag.tag or "##{@query}".eql? tag.tag
+              @results << item
+            end
+          end
+          # searches by token
+          if @query.eql? item.token
             @results << item
           end
-        end
-        # searches by token or location
-        if @query.eql? item.token or item.location.to_s.include? @query.to_s
-          @results << item
+          # searches by location
+          if item.location.to_s.include? @query.to_s
+            @results << item
+          end
         end
       end
     end
