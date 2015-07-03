1a5b34bbb90004fd5782d1e28e4f79d5a574f20c
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-07-03 12:10:37 -0400

    update_token_fix

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,7 +1,7 @@
 class TokenController < ApplicationController
   def update
     timestamp = cookies[:ip_timestamp]
-    if timestamp.nil? or timestamp.to_date > 1.hours.ago
+    if timestamp.nil? or timestamp.to_date < 1.hours.ago
       cookies.permanent[:ip] = request.remote_ip.to_s
       cookies.permanent[:ip_timestamp] = DateTime.current.to_s
       cookies.permanent[:token] = SecureRandom.urlsafe_base64
