f4fd3f2f77575f836b004511b90214beda30e89a
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-07-10 14:16:58 -0400

    propose_manifestos

--- app/controllers/manifestos_controller.rb
+++ app/controllers/manifestos_controller.rb
@@ -4,6 +4,12 @@ class ManifestosController < ApplicationController
   end
   
   def index
+    @manifestos_index = true
     @manifestos = Manifesto.proposed
+  end
+  
+  def create
+    @manifesto = Manifesto.new(params[:manifesto].permit(:body))
+    @manifesto.save if @manifesto.body.present?; redirect_to :back
   end
 end
