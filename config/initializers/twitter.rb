e97dd8ae959f5628732946fd337f9c32817aba80
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-07-23 20:36:08 -0400

    testing_twitter

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,10 @@
 
+if ENV['TWITTER_CONSUMER_KEY'].present?
+  puts "Twitter API keys found."
+else
+  puts "Could not find Twitter API keys."
+end
+
 $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
