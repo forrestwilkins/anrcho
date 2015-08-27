05b0bdc008020d6f8d7b67e596e44b73ef576f06
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-08-27 17:12:23 -0400

    time_ago

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+  def time_ago(_time_ago)
+    _time_ago = _time_ago + " ago"
+    if _time_ago.include? "about"
+    	_time_ago.slice! "about "
+    end
+    if _time_ago[0].to_i > 0 and _time_ago[1].to_i > 0
+      _time_ago = _time_ago[0..2] + _time_ago[3.._time_ago.size]
+    elsif _time_ago[0].to_i > 0
+      _time_ago = _time_ago[0..1] + _time_ago[2.._time_ago.size]
+    end
+    return _time_ago
+  end
+  
 	def random_color
 		rgb = []; 3.times { rgb << Random.rand(1..255) }
 		return rgb
