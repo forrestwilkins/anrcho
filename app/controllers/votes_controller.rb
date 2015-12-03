8850860d1a048699bc0f738ffbe001091904c393
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-12-03 14:47:53 -0500

    reverse_ratification

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -29,12 +29,21 @@ class VotesController < ApplicationController
   
   def reverse
     @vote = Vote.find_by_unique_token params[:token]
-    if @vote.down? and @vote.verified and not @vote.votes.find_by_token security_token
-      @vote.votes.create flip_state: 'down', token: security_token
-      if @vote.votes_to_reverse.zero?
-        @vote.proposal.update requires_revision: false
-        @vote.update verified: false
-        @vote.votes.destroy_all
+    if @vote.verified and not @vote.votes.find_by_token security_token
+      if @vote.up?
+        @vote.votes.create flip_state: 'down', token: security_token
+        if @vote.votes_to_reverse.zero?
+          @vote.proposal.update ratified: false
+          @vote.update verified: false
+          @vote.votes.destroy_all
+        end
+      elsif @vote.down?
+        @vote.votes.create flip_state: 'down', token: security_token
+        if @vote.votes_to_reverse.zero?
+          @vote.proposal.update requires_revision: false
+          @vote.update verified: false
+          @vote.votes.destroy_all
+        end
       end
     end
     redirect_to :back
